0b5b974e1889187c849906c097558f407d9cc3c3
JJ <youngje3431@gmail.com>
2021-09-09 09:29:22 +0900

    aspectj, Slf4j 버전 정보 수정.

--- src/main/java/com/yourplace/commons/json/ReadJsonFile.java
+++ src/main/java/com/yourplace/commons/json/ReadJsonFile.java
@@ -9,13 +9,19 @@ import java.util.List;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
-
+//JSON 파일을 읽어오기 위한 클래스.
 public class ReadJsonFile {
+	//리턴 타입이 String 리스트 타입이다.
 	public List<String> parser() throws IOException, ParseException {
+		//JSON-SIMPLE 라이브러리를 이용한다.
 		JSONParser parser = new JSONParser();
+		//IO 의 리더 클래스를 이용해 내 컴퓨터의 *.json을 가지는 파일을 읽어들인다.
+		//.gitignore 에 포함되어 있는 파일이기 때문에 GITHUB에는 PUSH 되어 있지 않다.
 		Reader reader = new FileReader(
 				"/Users/jaejaean/Desktop/KGitBankWebDev/MySpring/yourplace/src/main/resources/config/key.json");
+		//JSON 객체를 읽어들인다.
 		JSONObject jsonObject = (JSONObject) parser.parse(reader);
+		//읽어들인 객체에서 키값을 매개로 Value를 받아온다.
 		String accessKey = (String)jsonObject.get("accessKey");
 		String secretKey = (String)jsonObject.get("secretKey");
 		
